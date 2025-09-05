65db3892db2f84a01a478e36e616061990f58ce5
IsKJH <dkdlrh147@gmail.com>
2025-09-05 15:55:02 +0900

    init:[김지한] 프로젝트 생성

---
+++ src/shared/components/pages/Home.tsx
@@ -0,0 +1,13 @@
+import "react";
+
+function Home() {
+    return (
+        <>
+            <div>
+                홈이요
+            </div>
+        </>
+    )
+}
+
+export default Home;
---
+++ src/shared/routers/Router.tsx
@@ -0,0 +1,17 @@
+import "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../components/pages/Home.tsx";
+
+function Router() {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
+}
+
+export default Router;
